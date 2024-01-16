6e5df278fd4851a7196187cfbc52c0ee4af01ccf
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2024-01-16 09:30:58 +0900

    ICT2-38 Vite로 마이그레이션 & prettier세팅 & eslint세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    node: true,
+    es6: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -16,5 +19,10 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'no-undef': 'off',
+    'react/prop-types': 'warn',
   },
-}
+
+};
